234164bfd48cc5dd11833d245537ecc69045c437
LibereCode <libere-code@posteo.org>
2026-09-09 13:39:16 +0200

    feat(done): -> 07_structs

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -30,11 +30,17 @@ mod tests {
     use super::*;
 
     #[test]
+    ///Docs: [Listing 5-7](https://doc.rust-lang.org/book/ch05-01-defining-structs.html#listing-5-7)
     fn your_order() {
         let order_template = create_order_template();
 
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            count: 1,
+
+            ..order_template
+        };
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);

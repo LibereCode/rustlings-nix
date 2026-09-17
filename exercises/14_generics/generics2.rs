86b8c71114bb864a75cf6c88c79f082f79e10859
LibereCode <libere-code@posteo.org>
2026-09-17 14:55:30 +0200

    feat(done): 14_generics

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -1,18 +1,51 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
-    fn new(value: u32) -> Self {
+impl<T> Wrapper<T> {
+    fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let wrap = Wrapper::new("(w)Rapper");
+    println!("Hello, {}!\n", wrap.value);
+
+    // could just use this, why use ::new() ? -- Answer: convenience
+    let simpler = Wrapper {
+        value: "simpler use",
+    };
+    println!("Hello, {}!\n", simpler.value);
+
+    // From: <https://doc.rust-lang.org/book/ch10-01-syntax.html#in-method-definitions>
+    struct Point<Type> {
+        x: Type,
+        y: Type,
+    }
+    impl<T> Point<T> {
+        fn x(&self) -> &T {
+            &self.x
+        }
+        fn y(&self) -> &T {
+            &self.y
+        }
+        fn new(x: T, y: T) -> Self {
+            Point { x, y }
+        }
+    }
+    fn print_p(name: &str, p: Point<i32>) {
+        println!("{} = {{\n\tx = {},\n\ty = {},\n}}", name, p.x(), p.y());
+    }
+    let p1 = Point { x: 5, y: 10 };
+    print_p("p1", p1);
+
+    let p2 = Point::new(6, 7);
+    print_p("p2", p2);
 }
 
 #[cfg(test)]
